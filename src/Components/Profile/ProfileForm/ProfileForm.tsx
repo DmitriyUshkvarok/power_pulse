d12acf255d41aaeda4980abe0ef25d3cbc4437e9
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2023-11-18 21:02:41 +0200

    styles profile form

--- src/Components/Profile/ProfileForm/ProfileForm.tsx
+++ src/Components/Profile/ProfileForm/ProfileForm.tsx
@@ -84,225 +84,268 @@ const ProfileForm = () => {
         innerRef={formikRef}
       >
         {({ isValid, setFieldValue }) => (
-          <Form className={styles.form_step_one}>
-            <div className={styles.profile_form_group}>
-              <Field
-                className={styles.form_step_one_input}
-                type="text"
-                name="height"
-                aria-label="height"
-              />
-              <span className={styles.step_one_form_placeholder}>Height</span>
-              <ErrorMessage name="height">
-                {(msg) => <div className={styles.validation_error}>{msg}</div>}
-              </ErrorMessage>
+          <Form className={styles.form_profile}>
+            <div className={styles.form_block_one}>
+              <div className={styles.profile_form_group_one}>
+                <Field
+                  className={styles.form_profile_input_one}
+                  type="text"
+                  name="name"
+                  aria-label="name"
+                />
+                <span className={styles.span_one_form_placeholder}>Name</span>
+                <ErrorMessage name="name">
+                  {(msg) => (
+                    <div className={styles.validation_error}>{msg}</div>
+                  )}
+                </ErrorMessage>
+              </div>
+              <div className={styles.profile_form_group_one}>
+                <Field
+                  className={styles.form_profile_input_one}
+                  type="email"
+                  name="email"
+                  aria-label="email"
+                />
+                <span className={styles.span_one_form_placeholder}>Email</span>
+                <ErrorMessage name="email">
+                  {(msg) => (
+                    <div className={styles.validation_error}>{msg}</div>
+                  )}
+                </ErrorMessage>
+              </div>
             </div>
-            <div className={styles.profile_form_group}>
-              <Field
-                className={styles.form_step_one_input}
-                type="text"
-                name="currentWeight"
-                aria-label="Current Weight"
-              />
-              <span className={styles.step_one_form_placeholder}>
-                Current Weight
-              </span>
-              <ErrorMessage name="currentWeight">
-                {(msg) => <div className={styles.validation_error}>{msg}</div>}
-              </ErrorMessage>
-            </div>
-            <div className={styles.profile_form_group}>
-              <Field
-                className={styles.form_step_one_input}
-                type="text"
-                name="desiredWeight"
-                aria-label="Desired Weight"
-              />
-              <span className={styles.step_one_form_placeholder}>
-                Desired Weight
-              </span>
-              <ErrorMessage name="desiredWeight">
-                {(msg) => <div className={styles.validation_error}>{msg}</div>}
-              </ErrorMessage>
-            </div>
-            <div className={styles.profile_form_group}>
-              <Field
-                className={styles.form_step_one_input}
-                type="text"
-                name="birthday"
-                aria-label="birthday"
-              />
-              <span className={styles.step_one_form_placeholder}>Birthday</span>
-              <Image
-                src="/calendar.svg"
-                alt="calendar icon"
-                width={18}
-                height={18}
-                className={styles.calendar_icon}
-                onClick={handleCalendarToggle}
-              />
-              {isCalendarOpen && (
-                <div
-                  className={styles.calendar_backdrop}
-                  onClick={clickBackdrop}
-                >
-                  <div className={styles.calendarContainer}>
-                    <Calendar
-                      className={styles.customCalendar}
-                      onChange={(selectedDate) => {
-                        if (selectedDate instanceof Date) {
-                          setFieldValue(
-                            'birthday',
-                            selectedDate.toLocaleDateString('en-GB', {
-                              day: 'numeric',
-                              month: 'numeric',
-                              year: 'numeric',
-                            })
-                          );
-                          setDate(selectedDate);
-                        }
-                      }}
-                      value={date}
-                    />
+            <div className={styles.form_block_two}>
+              <div className={styles.profile_form_group_two}>
+                <Field
+                  className={styles.form_profile_input_two}
+                  type="text"
+                  name="height"
+                  aria-label="height"
+                />
+                <span className={styles.span_one_form_placeholder}>Height</span>
+                <ErrorMessage name="height">
+                  {(msg) => (
+                    <div className={styles.validation_error_two}>{msg}</div>
+                  )}
+                </ErrorMessage>
+              </div>
+              <div className={styles.profile_form_group_two}>
+                <Field
+                  className={styles.form_profile_input_two}
+                  type="text"
+                  name="currentWeight"
+                  aria-label="Current Weight"
+                />
+                <span className={styles.span_one_form_placeholder}>
+                  Current Weight
+                </span>
+                <ErrorMessage name="currentWeight">
+                  {(msg) => (
+                    <div className={styles.validation_error_two}>{msg}</div>
+                  )}
+                </ErrorMessage>
+              </div>
+              <div className={styles.profile_form_group_two}>
+                <Field
+                  className={styles.form_profile_input_two}
+                  type="text"
+                  name="desiredWeight"
+                  aria-label="Desired Weight"
+                />
+                <span className={styles.span_one_form_placeholder}>
+                  Desired Weight
+                </span>
+                <ErrorMessage name="desiredWeight">
+                  {(msg) => (
+                    <div className={styles.validation_error_two}>{msg}</div>
+                  )}
+                </ErrorMessage>
+              </div>
+              <div className={styles.profile_form_group_two}>
+                <Field
+                  className={styles.form_profile_input_two}
+                  type="text"
+                  name="birthday"
+                  aria-label="birthday"
+                />
+                <span className={styles.span_one_form_placeholder}>
+                  Birthday
+                </span>
+                <Image
+                  src="/calendar.svg"
+                  alt="calendar icon"
+                  width={18}
+                  height={18}
+                  className={styles.calendar_icon}
+                  onClick={handleCalendarToggle}
+                />
+                {isCalendarOpen && (
+                  <div
+                    className={styles.calendar_backdrop}
+                    onClick={clickBackdrop}
+                  >
+                    <div className={styles.calendarContainer}>
+                      <Calendar
+                        className={styles.customCalendar}
+                        onChange={(selectedDate) => {
+                          if (selectedDate instanceof Date) {
+                            setFieldValue(
+                              'birthday',
+                              selectedDate.toLocaleDateString('en-GB', {
+                                day: 'numeric',
+                                month: 'numeric',
+                                year: 'numeric',
+                              })
+                            );
+                            setDate(selectedDate);
+                          }
+                        }}
+                        value={date}
+                      />
+                    </div>
                   </div>
+                )}
+                <ErrorMessage name="birthday">
+                  {(msg) => (
+                    <div className={styles.validation_error_two}>{msg}</div>
+                  )}
+                </ErrorMessage>
+              </div>
+            </div>
+            <label className={styles.label_radio}>Blood:</label>
+            <div className={styles.profile_form_group_sex_and_blood_group}>
+              <div className={styles.form_block_three}>
+                <div className={styles.profile_form_group_three}>
+                  <Field
+                    className={styles.radio_btn}
+                    type="radio"
+                    name="bloodGroup"
+                    value="1"
+                  />
+                  <span className={styles.radio_text}>1</span>
                 </div>
-              )}
-              <ErrorMessage name="birthday">
-                {(msg) => <div className={styles.validation_error}>{msg}</div>}
-              </ErrorMessage>
-            </div>
-            <div className={styles.profile_form_group}>
-              <label className={styles.label_radio}>Blood:</label>
-              <div className={styles.form_group_text_and_btn}>
-                <Field
-                  className={styles.radio_btn}
-                  type="radio"
-                  name="bloodGroup"
-                  value="A"
-                />
-                <span className={styles.radio_text}>1</span>
-              </div>
-              <div className={styles.profile_form_group}>
-                <Field
-                  className={styles.radio_btn}
-                  type="radio"
-                  name="bloodGroup"
-                  value="B"
-                />
-                <span className={styles.radio_text}>2</span>
-              </div>
-              <div className={styles.profile_form_group}>
-                <Field
-                  className={styles.radio_btn}
-                  type="radio"
-                  name="bloodGroup"
-                  value="AB"
-                />
-                <span className={styles.radio_text}>3</span>
-              </div>
-              <div className={styles.profile_form_group}>
-                <Field
-                  className={styles.radio_btn}
-                  type="radio"
-                  name="bloodGroup"
-                  value="O"
-                />
-                <span className={styles.radio_text}>4</span>
-              </div>
-              <ErrorMessage name="bloodGroup">
-                {(msg) => <div className={styles.validation_error}>{msg}</div>}
-              </ErrorMessage>
-            </div>
 
-            <label className={styles.label_radio}>Sex:</label>
-            <div className={styles.profile_form_group}>
-              <Field
-                className={styles.radio_btn}
-                type="radio"
-                name="sex"
-                value="Male"
-              />
-              <span className={styles.radio_text}>Male</span>
+                <div className={styles.profile_form_group_three}>
+                  <Field
+                    className={styles.radio_btn}
+                    type="radio"
+                    name="bloodGroup"
+                    value="2"
+                  />
+                  <span className={styles.radio_text}>2</span>
+                </div>
+                <div className={styles.profile_form_group_three}>
+                  <Field
+                    className={styles.radio_btn}
+                    type="radio"
+                    name="bloodGroup"
+                    value="3"
+                  />
+                  <span className={styles.radio_text}>3</span>
+                </div>
+                <div className={styles.profile_form_group_three}>
+                  <Field
+                    className={styles.radio_btn}
+                    type="radio"
+                    name="bloodGroup"
+                    value="4"
+                  />
+                  <span className={styles.radio_text}>4</span>
+                </div>
+              </div>
+              <div className={styles.form_block_four}>
+                <div className={styles.profile_form_group_four}>
+                  <Field
+                    className={styles.radio_btn}
+                    type="radio"
+                    name="sex"
+                    value="Male"
+                  />
+                  <span className={styles.radio_text}>Male</span>
+                </div>
+                <div className={styles.profile_form_group_four}>
+                  <Field
+                    className={styles.radio_btn}
+                    type="radio"
+                    name="sex"
+                    value="Female"
+                  />
+                  <span className={styles.radio_text}>Female</span>
+                </div>
+              </div>
             </div>
-            <div className={styles.profile_form_group}>
-              <Field
-                className={styles.radio_btn}
-                type="radio"
-                name="sex"
-                value="Female"
-              />
-              <span className={styles.radio_text}>Female</span>
+            <div className={styles.form_group_five}>
+              <div className={styles.profile_form_group_five}>
+                <Field
+                  className={styles.radio_btn_five}
+                  type="radio"
+                  name="levelActivity"
+                  value="Sedentary lifestyle"
+                />
+                <span className={styles.radio_text}>
+                  Sedentary lifestyle (little or no physical activity)
+                </span>
+              </div>
+              <div className={styles.profile_form_group_five}>
+                <Field
+                  className={styles.radio_btn_five}
+                  type="radio"
+                  name="levelActivity"
+                  value="Light activity"
+                />
+                <span className={styles.radio_text}>
+                  Light activity (light exercises/sports 1-3 days per week)
+                </span>
+              </div>
+              <div className={styles.profile_form_group_five}>
+                <Field
+                  className={styles.radio_btn_five}
+                  type="radio"
+                  name="levelActivity"
+                  value="Moderately active"
+                />
+                <span className={styles.radio_text}>
+                  Moderately active (moderate exercises/sports 3-5 days per
+                  week)
+                </span>
+              </div>
+              <div className={styles.profile_form_group_five}>
+                <Field
+                  className={styles.radio_btn_five}
+                  type="radio"
+                  name="levelActivity"
+                  value="Very active"
+                />
+                <span className={styles.radio_text}>
+                  Very active (intense exercises/sports 6-7 days per week)
+                </span>
+              </div>
+              <div className={styles.profile_form_group_five}>
+                <Field
+                  className={styles.radio_btn_five}
+                  type="radio"
+                  name="levelActivity"
+                  value="Extremely active"
+                />
+                <span className={styles.radio_text}>
+                  Extremely active (very strenuous exercises/sports and physical
+                  work)
+                </span>
+              </div>
             </div>
-            <ErrorMessage name="sex">
-              {(msg) => <div className={styles.validation_error}>{msg}</div>}
-            </ErrorMessage>
-            <label className={styles.label_radio}>Level of Activity:</label>
-            <div className={styles.profile_form_group}>
-              <Field
-                className={styles.radio_btn}
-                type="radio"
-                name="levelActivity"
-                value="Sedentary lifestyle"
-              />
-              <span className={styles.radio_text}>
-                Sedentary lifestyle (little or no physical activity)
-              </span>
-            </div>
-            <div className={styles.profile_form_group}>
-              <Field
-                className={styles.radio_btn}
-                type="radio"
-                name="levelActivity"
-                value="Light activity"
-              />
-              <span className={styles.radio_text}>
-                Light activity (light exercises/sports 1-3 days per week)
-              </span>
-            </div>
-            <div className={styles.profile_form_group}>
-              <Field
-                className={styles.radio_btn}
-                type="radio"
-                name="levelActivity"
-                value="Moderately active"
-              />
-              <span className={styles.radio_text}>
-                Moderately active (moderate exercises/sports 3-5 days per week)
-              </span>
-            </div>
-            <div className={styles.profile_form_group}>
-              <Field
-                className={styles.radio_btn}
-                type="radio"
-                name="levelActivity"
-                value="Very active"
-              />
-              <span className={styles.radio_text}>
-                Very active (intense exercises/sports 6-7 days per week)
-              </span>
-            </div>
-            <div className={styles.profile_form_group}>
-              <Field
-                className={styles.radio_btn}
-                type="radio"
-                name="levelActivity"
-                value="Extremely active"
-              />
-              <span className={styles.radio_text}>
-                Extremely active (very strenuous exercises/sports and physical
-                work)
-              </span>
-            </div>
-            <ErrorMessage name="levelActivity">
-              {(msg) => <div className={styles.validation_error}>{msg}</div>}
-            </ErrorMessage>
             <button
-              className={styles.link_next}
+              className={styles.btn_save}
               type="submit"
               disabled={!isValid}
             >
               <span>Save </span>
             </button>
+            {!isValid && (
+              <p className={styles.errorText}>
+                Please fill in all the required fields.
+              </p>
+            )}
           </Form>
         )}
       </Formik>

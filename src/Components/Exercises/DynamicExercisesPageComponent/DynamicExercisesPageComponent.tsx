ea1e750f85e2ac8fad6ee2610fe46a18aaf3f01b
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2024-04-01 19:43:47 +0300

    test modal

--- src/Components/Exercises/DynamicExercisesPageComponent/DynamicExercisesPageComponent.tsx
+++ src/Components/Exercises/DynamicExercisesPageComponent/DynamicExercisesPageComponent.tsx
@@ -27,9 +27,9 @@ const DynamicExercisesPageComponent = ({ id }: PageId) => {
       >
         {id}
       </Link>
-      <Modal>
+      {/* <Modal>
         <AddExercisesForm id={id} />
-      </Modal>
+      </Modal> */}
     </div>
   );
 };
